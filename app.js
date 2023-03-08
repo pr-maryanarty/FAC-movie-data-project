476cacf9355581de65b6a1b8b5d2a5b29a0e12c0
pr-maryanarty <maryana.dementeva@gmail.com>
2023-03-09 01:00:43 +0400

    movie data movie card added

--- app.js
+++ app.js
@@ -33,3 +33,64 @@ let movieData = {
     cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
   },
 };
+
+let curMovie = movieData["The Darjeeling Limited"]; // curMovie <-> {"The Darjeeling Limited": { ... }}
+
+// const searchButton = document.querySelector("#search").value.toLowerCase();
+// searchButton.addEventListener("input", function () {
+
+// }
+
+// }
+//
+
+// function renderMovies(moviesArray) {
+//   let rootDiv = document.getElementById("result");
+// }
+
+// renderMovies(movieData);
+// let movies = Object.keys(movieData);
+// console.log(movieData["The Grand Budapest Hotel"].rating);
+// console.log(movies);
+// console.log(movies[2]);
+
+// let name = document.querySelector(movieData)`movie ${movie}`;
+// let result =
+
+function renderMovieGallery(movieArray) {
+  // for aMovie of movieArray
+  //    renderMovieCard(aMovie)
+}
+
+function renderMovieCard(movie) {
+  let plot = movie.plot;
+  let cast = movie.cast;
+  let year = movie.year;
+  let runtime = movie.runtime;
+  let rating = movie.rating;
+
+  const movieCard = document.createElement("div");
+
+  const plotDescription = document.createElement("p");
+  plotDescription.innerHTML = plot;
+  const castDescription = document.createElement("p");
+  castDescription.innerHTML = cast;
+  const yearDescription = document.createElement("p");
+  yearDescription.innerHTML = year;
+  const runtimeDescription = document.createElement("p");
+  runtimeDescription.innerHTML = runtime;
+  const ratingDescription = document.createElement("p");
+  ratingDescription.innerHTML = rating;
+  //
+
+  const content = document.querySelector("#content");
+  content.append(movieCard);
+  movieCard.append(plotDescription);
+  movieCard.append(castDescription);
+  movieCard.append(yearDescription);
+  movieCard.append(runtimeDescription);
+  movieCard.append(ratingDescription);
+}
+
+// renderMovieCard(curMovie);
+renderMovieGallery(movieData);
