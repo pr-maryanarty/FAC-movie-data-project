6a0b6dd7b94da40b1198a01dcc911bba1d7b121d
pr-maryanarty <maryana.dementeva@gmail.com>
2023-03-15 23:45:13 +0400

    prepared for sorting

--- app.js
+++ app.js
@@ -34,11 +34,50 @@ let movieData = {
   },
 };
 
+// let moviesArray = [
+//   {
+//    title: "The Darjeeling Limited";
+//     plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+//     cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+//     runtime: 151,
+//     rating: 7.2,
+//     year: 2007,
+//   },
+//   {
+//    title: "The Royal Tenenbaums";
+//     plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+//     rating: 7.6,
+//     year: 2001,
+//     cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+//     runtime: 170,
+//   },
+// ];
+
 const content = document.querySelector("#content");
 // const searchButton = document.querySelector("#search").value.toLowerCase();
 // searchButton.addEventListener("input", function () {
 
+function mapToArray(movieData) {
+  let arrayForMovies = [];
+  for (const movieTitle in movieData) {
+    let curMovie = movieData[movieTitle];
+    let movieInfo = {
+      title: movieTitle,
+      rating: curMovie.rating,
+      year: curMovie.year,
+      runtime: curMovie.runtime,
+      plot: curMovie.plot,
+      cast: curMovie.cast,
+    };
+    arrayForMovies.push(movieInfo);
+  }
+  return arrayForMovies;
+}
+
+let moviesArray = mapToArray(movieData);
+
 function renderMovieCard(movie) {
+  let title = movie.title;
   let plot = movie.plot;
   let cast = movie.cast;
   let year = movie.year;
@@ -46,64 +85,48 @@ function renderMovieCard(movie) {
   let rating = movie.rating;
 
   const movieCard = document.createElement("div");
+  const titleElement = document.createElement("h1");
+  titleElement.innerHTML = title;
+  const plotElement = document.createElement("p");
+  plotElement.innerHTML = plot;
+  const ratingElement = document.createElement("p");
+  ratingElement.innerHTML = "⭐️ " + rating;
+  const yearElement = document.createElement("p");
+  yearElement.innerHTML = "realised in " + year;
+  const runtimeElement = document.createElement("p");
+  runtimeElement.innerHTML = runtime + " mins";
+  const castElement = document.createElement("p");
+  castElement.innerHTML = "cast: " + cast;
 
-  const plotDescription = document.createElement("p");
-  plotDescription.innerHTML = plot;
-  const ratingDescription = document.createElement("p");
-  ratingDescription.innerHTML = "⭐️ " + rating;
-  const yearDescription = document.createElement("p");
-  yearDescription.innerHTML = "realised in " + year;
-  const runtimeDescription = document.createElement("p");
-  runtimeDescription.innerHTML = runtime + " mins";
-  const castDescription = document.createElement("p");
-  castDescription.innerHTML = "cast " + cast;
-
-  //
   content.append(movieCard);
-  movieCard.append(plotDescription);
-  movieCard.append(ratingDescription);
-  movieCard.append(yearDescription);
-  movieCard.append(runtimeDescription);
-  movieCard.append(castDescription);
+  movieCard.append(titleElement);
+  movieCard.append(plotElement);
+  movieCard.append(ratingElement);
+  movieCard.append(yearElement);
+  movieCard.append(runtimeElement);
+  movieCard.append(castElement);
   movieCard.classList.add("movieCard");
-  plotDescription.classList.add("elementOfMoviePlot");
-  castDescription.classList.add("elementOfMovieCast");
-  yearDescription.classList.add("elementOfMovieYear");
-  runtimeDescription.classList.add("elementOfMovieRuntime");
-  ratingDescription.classList.add("elementOfMovieRating");
+  titleElement.classList.add("elementOfMovieTitle");
+  plotElement.classList.add("elementOfMoviePlot");
+  castElement.classList.add("elementOfMovieCast");
+  yearElement.classList.add("elementOfMovieYear");
+  runtimeElement.classList.add("elementOfMovieRuntime");
+  ratingElement.classList.add("elementOfMovieRating");
 }
 
-function renderMovieData() {
+function renderMoviesArray() {
   content.innerHTML = "";
-  for (const movieTitle in movieData) {
-    let curMovie = movieData[movieTitle];
-    renderMovieCard(curMovie);
+  for (const movie of moviesArray) {
+    renderMovieCard(movie);
   }
 }
 
-renderMovieData();
-
-//movieData["Harry potter"] = harryPotterMovie;
-// movieData.push("Harry potter", harryPotterMovie)
-
-// let hpRatingInputElement = document.querySelector("#rating");
-// hpRatingValue = hpRatingInputElement.value;
-
-// let hpPlotInputElement = document.querySelector("#plot");
-// hpPlotValue = hpPlotInputElement.value;
-
-// let harryPotterMovie = {
-//   rating: hpRatingValue,
-//   plot: hpPlotValue,
-// };
-
-// movieData["Harry Potter"] = harryPotterMovie;
+renderMoviesArray();
 
 let submitNewMovie = document.querySelector(".submitMovie");
 submitNewMovie.addEventListener("click", function () {
-  let movieTitle = document.getElementById("movieTitle").value;
-
-  let movie = {
+  let newMovie = {
+    title: document.getElementById("movieTitle").value,
     rating: document.getElementById("rating").value,
     runtime: document.getElementById("runtime").value,
     year: document.getElementById("year").value,
@@ -111,12 +134,13 @@ submitNewMovie.addEventListener("click", function () {
     cast: document.getElementById("cast").value,
   };
 
-  movieData[movieTitle] = movie;
-  renderMovieData();
+  moviesArray.push(newMovie);
+  renderMoviesArray();
+  //close the form
   document.querySelector(".addNewMovie").classList.remove("show");
 });
 
 const formAddMovie = document.querySelector("#addNewMovie");
 formAddMovie.addEventListener("click", function () {
-  let formPopUp = document.querySelector(".addNewMovie").classList.add("show");
+  document.querySelector(".addNewMovie").classList.add("show");
 });
